6ff943ac861610678834a94731dc1731fe084c70
ziyuan <hanziyuan08@outlook.com>
2019-01-06 23:39:23 +0800

    upgrade junit to 5

--- src/test/java/com/github/bpazy/dingappender/DingAppenderTest.java
+++ src/test/java/com/github/bpazy/dingappender/DingAppenderTest.java
@@ -2,7 +2,7 @@ package com.github.bpazy.dingappender;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author ziyuan
